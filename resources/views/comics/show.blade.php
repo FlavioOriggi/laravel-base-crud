342de1efd4926a61bfa4f7dcd6d14bb98033e824
Flavio <flavio.origgi@outlook.it>
2021-09-10 01:20:14 +0200

    fatto il return da show ad home/comics item

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -12,12 +12,18 @@
 
         <div class="row">
             <div class="col-m-6">
-                <img src="{{ $comic->thumb }}" alt="">
+                <img src="{{ $comic->thumb }}" alt="{{ $comic->thumb }}">
             </div>
             <div class="col-m-6">
-                <p></p>
+                <p> {{ $comic->series }} </p>
             </div>
         </div>
+
+        <button type="button" class="btn btn-primary">
+            {{ $comic->price }} <span class="badge bg-secondary">€</span>
+        </button>
+
+        
     </div>
 
     
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,7 @@
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Comics Item</a>
+                <a class="nav-link" href="{{ route('comics.index')}}">Comics Item</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Pricing</a>
